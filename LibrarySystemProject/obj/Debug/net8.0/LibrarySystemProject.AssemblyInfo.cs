2967d5c168d316199fa8a642511a9304cf13f539
Ahmedli Aqshin Araz <Ahmed_hb29@itstep.edu.az>
2025-03-03 19:32:15 +0400

    FluentValidation

--- LibrarySystemProject/obj/Debug/net8.0/LibrarySystemProject.AssemblyInfo.cs
+++ LibrarySystemProject/obj/Debug/net8.0/LibrarySystemProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySystemProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68382703f6842988d995eac17ff2523c5adbf309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f131e0a25a46b43b39f2880d57651e18b1277b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySystemProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrarySystemProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
